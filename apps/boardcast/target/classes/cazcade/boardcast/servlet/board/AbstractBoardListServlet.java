816e2e43faa571e3add5b96f66a00bb12d0e21c1
Neil Ellis <neilellis@cazcade.com>
2013-01-09 23:42:19 +0000

    Removing junk.

--- apps/boardcast/target/classes/cazcade/boardcast/servlet/board/AbstractBoardListServlet.java
+++
@@ -1,9 +0,0 @@
-package cazcade.boardcast.servlet.board;
-
-import cazcade.boardcast.servlet.AbstractBoardcastServlet;
-
-/**
- * @author neilellis@cazcade.com
- */
-public class AbstractBoardListServlet extends AbstractBoardcastServlet {
-}
